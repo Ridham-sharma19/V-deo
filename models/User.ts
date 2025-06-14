f0422d182f4fa7f9c06111a1c01cecc2569abc6d
ridham sharma <ridham.sharmarce@gmail.com>
2025-06-14 20:08:49 +0530

    feat:User-schema-added

---
+++ models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, models, model } from "mongoose";
+import bcrypt from "bcryptjs";
+import { unique } from "next/dist/build/utils";
+
+export interface Iuser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<Iuser>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save',async function(next){
+    if(this.isModified("password")){
+        this.password=await bcrypt.hash(this.password,10);
+    }
+
+})
+
+const User=models?.User||model<Iuser>("User",userSchema);
+
+export default User;
